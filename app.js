6d12e0e91eb26b8d8e8b6ff03e5ef5c2514a1039
jbelmaro <jbelmaro@gmail.com>
2017-07-26 21:28:29 +0000

    Commit con nodejs funcionando y la plantilla cargada

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-const express = require('express')
-const app = express()
-
+var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -10,10 +8,11 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,12 +20,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/nodeGarden.js/images',express.static(path.join(__dirname, '/public/images')));
-app.use('/nodeGarden.js/js',express.static(path.join(__dirname, '/public/js')));
-app.use('/nodeGarden.js/css',express.static(path.join(__dirname, '/public/css')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/nodeGarden.js', index);
-app.use('/nodeGarden.js/users', users);
+app.use('/', index);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -47,7 +44,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
