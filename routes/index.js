85433cc4c918c89a729241f8558525fb655bdce2
jbelmaro <jbelmaro@gmail.com>
2017-08-01 17:08:56 +0200

    Se añade oauth a index

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
-var express = require('express');
-var router = express.Router();
+var app = require('express');
+var router = app.Router();
+var oauthserver = require('oauth2-server');
+
+app.oauth = oauthserver({
+	model: require('../model.js'),
+	grants: ['password'],
+	debug: true
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', app.oauth.authorise(), function(req, res, next) {
   res.render('index', { title: 'Garden Project JB' });
   res.end();
 });
